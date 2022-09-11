73a7442383b039168a85bb53105493ca9352fced
Mansur <mansurdzukalaev@gmail.com>
2022-09-11 20:31:22 +0300

    генератор паролей

--- generate-password.js
+++ generate-password.js
@@ -1,11 +1,29 @@
-function generatePassword(length) {
-  const alphabet = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
-  let newPassword = '';
-
-  for(let i = 0; i < length; i++) {
-    const randomSymbol = alphabet[0]; // я умею брать только первый символ из строки, а нужно взять один случайный символ :(
-    newPassword += randomSymbol;
+function generatePassword(length, level = "easy", count = 1) {
+  const alphabet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  const digits = "0123456789";
+  const symbols = "!#$%&()*+,-./:;<=>?@[]^_{|}~";
+  let res = alphabet;
+  const passwords = [];
+  if (level === "medium") {
+    res += digits;
+  } else if (level === "hard") {
+    res += digits + symbols;
   }
-
-  return newPassword;
+  for (let j = 0; j < count; j++) {
+    let newPassword = "";
+    for (let i = 0; i < length; i++) {
+      const randomSymbol = res[Math.floor(Math.random() * (res.length - 1))];
+      newPassword += randomSymbol;
+    }
+    if (passwords.indexOf(newPassword) != -1) {
+      j--;
+    } else {
+      passwords.push(newPassword);
+    }
+  }
+  return passwords;
 }
+
+console.log(generatePassword(4, "easy"));
+console.log(generatePassword(6, "medium", 2));
+console.log(generatePassword(7, "hard", 10));
